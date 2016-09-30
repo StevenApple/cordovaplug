4abb5d50ab532c669859f7bdfa1347e13f400643
Steven <steven@anappleapp.com>
2016-09-30 12:33:18 +0200

    Initial Commit

---
+++ plugtest2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  plugtest2
+//
+//  Created by steven on 9/30/16.
+//  Copyright © 2016 Mari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
